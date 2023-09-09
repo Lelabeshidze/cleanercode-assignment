82b394eb9f884ad40b71547dbcab3ac1ccf0605d
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-09-09 22:57:35 +0400

    added styles

--- src/app/posts/PostList.jsx
+++ src/app/posts/PostList.jsx
@@ -5,13 +5,18 @@ async function getPosts() {
 export default async function PostList() {
   const posts = await getPosts();
   return (
-    <>
+    <div className={`w-full flex space-x-4 my-8 ml-10`}>
       {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
-        </div>
+        <>
+          <div
+            key={post.id}
+            className={`max-w-sm rounded overflow-hidden shadow-lg w-80 flex-1 py-10 bg-sky-600`}
+          >
+            <h3 className={`text-gray-700 text-base text-sky-100 text-center`}>{post.title}</h3>
+            <p className={`text-gray-700 text-base py-10 text-sky-100 text-center`}> {post.content}</p>
+          </div>
+        </>
       ))}
-    </>
+    </div>
   );
 }
