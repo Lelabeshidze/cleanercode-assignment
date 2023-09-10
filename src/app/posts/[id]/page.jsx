d94d729d7e68778b46dd93a903204d28253de67c
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-09-10 23:26:22 +0400

    edited styles

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -5,9 +5,15 @@ async function getPost(id) {
 export default async function PostDetails({ params }) {
   const post = await getPost(params.id);
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    <div
+      className={`max-w-sm rounded overflow-hidden shadow-lg  my-8  py-10 bg-sky-600 mx-auto `}
+    >
+      <h1 className={`text-gray-700 text-base text-sky-100 text-center`}>
+        {post.title}
+      </h1>
+      <p className={`text-gray-700 text-base py-10 text-sky-100 text-center`}>
+        {post.content}
+      </p>
     </div>
   );
 }
