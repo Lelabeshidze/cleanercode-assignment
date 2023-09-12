18a7c8d948a15d67113af4166fe562ac5331733d
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2023-09-12 20:33:37 +0400

    .

--- src/app/posts/PostList.tsx
+++ src/app/posts/PostList.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 async function getPosts() {
   const host = "http://localhost:4000";
-  const res = await fetch(host + "/posts");
+  const res = await fetch(process.env.URL + "/posts");
   return res.json();
 }
 
